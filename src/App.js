fa360a791d1ff5a088f5da44dd7b066b2ad17758
pictor01 <paas84@yahoo.com>
2025-01-15 12:56:13 -0500

    Paul's First Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import PaulAAlba from "./components/PaulAAlba";
 
 function App() {
   return (
     <div className="App">
       <h1>Alba Gold System</h1>
+
+      <PaulAAlba />
     </div>
   );
 }
---
+++ src/components/PaulAAlba.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./PaulAAlba.css";
+
+const PaulAAlba = () => {
+  return (
+    <div className="paul-alba-container">
+      <a
+        href="https://www.albagoldsystems.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="paul-alba-text"
+      >
+        Paul A Alba
+      </a>
+    </div>
+  );
+};
+
+export default PaulAAlba;
