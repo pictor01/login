58efd220662f20ee81aa6d56546154ae8d6f5a4a
pictor01 <paas84@yahoo.com>
2025-01-16 16:20:54 -0500

    i did this changes

--- src/components/PaulAAlba.jsx
+++ src/components/PaulAAlba.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import "./PaulAAlba.css";
 
-const PaulAAlba = () => {
-  return (
-    <div className="paul-alba-container">
-      <a
-        href="https://www.albagoldsystems.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="paul-alba-text"
-      >
-        Paul A Alba
-      </a>
-    </div>
-  );
-};
+const PaulAAlba = () => (
+  <div className="paul-alba-container">
+    <a
+      href="https://www.albagoldsystems.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="paul-alba-text"
+    >
+      Paul A Alba
+    </a>
+  </div>
+);
 
 export default PaulAAlba;
